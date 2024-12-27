8c3340e9785afd35964096659fc9100e13aa35ae
AdiDharmaPratama <adidarma1404@gmail.com>
2024-12-27 21:11:04 +0700

    Add folder and new file

---
+++ Assets/Style & Script/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typeEffect', {
+    strings: ['240210007', '<strong>Web Develover</strong>'],
+    typeSpeed: 40,
+    backSpeed: 30,
+    smartBackspace: true,
+    loop: true
+});
